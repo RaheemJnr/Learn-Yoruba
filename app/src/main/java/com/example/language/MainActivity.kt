dd0ce9659f0de5ffd6c04bac43053616f30ebb89
Raheem Jnr <mumedian6@gmail.com>
2023-03-10 17:50:31 +0100

    fixed audio focus error that mad app crash when trying to play audio

--- app/src/main/java/com/example/language/MainActivity.kt
+++ app/src/main/java/com/example/language/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent() {
+        setContent {
             LanguageTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(

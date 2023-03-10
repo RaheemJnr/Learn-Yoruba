bd76b5267d7a9084710235656f9f9281c91c73b1
Raheem Jnr <mumedian6@gmail.com>
2023-03-10 20:06:06 +0100

    fixed audio focus error that mad app crash when trying to play audio

--- app/src/main/java/com/example/language/ui/screen/ListItemLayout.kt
+++ app/src/main/java/com/example/language/ui/screen/ListItemLayout.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
